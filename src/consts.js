c53cb38fc96d77ccbef3d3cdce4e66eec2b66db9
David Roldán <deroldan98@gmail.com>
2023-10-06 14:51:09 -0500

    Update template

--- src/consts.js
+++ src/consts.js
@@ -1,115 +1,43 @@
 export const DEFAULT_EDITOR_DATA = {
-  time: 1696544236413,
+  time: 1696621622442,
   blocks: [
     {
-      id: "header",
-      type: "image",
-      data: {
-        file: {
-          url: "https://editor-testing-images.s3.us-east-2.amazonaws.com/logo-azul.svg",
-        },
-        caption: "",
-        withBorder: false,
-        stretched: false,
-        withBackground: false,
-      },
-    },
-    {
-      id: "LDxJExmOcI",
+      id: "kRoBQ7d-Hx",
       type: "paragraph",
-      data: { text: "lD: 09000093367" },
+      data: { text: '<img class="image-tool__image-picture" src="https://editor-testing-images.s3.us-east-2.amazonaws.com/logo-azul.svg" style="width: 190px;height: 60px;border: none;">' },
       tunes: { anyTuneName: { alignment: "left" } },
     },
     {
-      id: "hZg4Fmlu0S",
-      type: "table",
-      data: {
-        withHeadings: false,
-        content: [
-          [
-            '<span style="font-size: medium; text-align: left;">Boynton Beach, State of Florida, USA.</span>',
-            '<span style="font-size: medium; text-align: left;">Abril 19 de 2023</span>',
-          ],
-        ],
-      },
-      tunes: { anyTuneName: { alignment: "center" } },
-    },
-    { id: "vD9sLrk4g6", type: "breakLine", data: { divider: false } },
-    {
-      id: "LL36Ax8THP",
+      id: "kRoBQ7d-Hx",
       type: "paragraph",
-      data: { text: "HACE CONSTAR:" },
-      tunes: { anyTuneName: { alignment: "center" } },
+      data: { text: '<font style="color: rgb(0, 112, 255);">{{TITLE}}</font>' },
+      tunes: { anyTuneName: { alignment: "left" } },
     },
-    { id: "vD9sLrk4g6", type: "breakLine", data: { divider: false } },
+    { id: "s5eAmD4Pbj", type: "breakLine", data: { divider: false } },
     {
-      id: "x-yy1aF1AP",
+      id: "piExn5vPQt",
       type: "paragraph",
       data: {
-        text: "Que Pepito con cédula de ciudadanía número 1.005.206.910 de Armenia, labora en nuestra empresa HLMBUSINESS LLC con IP P09000093367 del estado de la Florida (USA) y EIN 27-1312352 del IRS (Internal Revenue Service), como Desarrollador de Software con un contrato por prestación de servicios desde Febrero de 2021, hasta la fecha. Devengando en promedio $1,200 dólares mensuales realizando las siguientes gestiones:",
+        text: 'This is to certify that <font style="color: rgb(0, 112, 255);">{{name}}</font>&nbsp;is presently employed in <font style="color: rgb(0, 112, 255);">{{organization}}</font>&nbsp;as\nof <font style="color: rgb(0, 112, 255);">{{current_date}}</font>, since <font style="color: rgb(0, 112, 255);">{{start_date}}</font> or for a period of <font style="color: rgb(0, 112, 255);">{{total_time}}</font>.',
       },
       tunes: { anyTuneName: { alignment: "left" } },
     },
-    { id: "D_LqEjzVrd", type: "breakLine", data: { divider: false } },
+    { id: "bWhN6BzG9E", type: "breakLine", data: { divider: false } },
     {
-      id: "SJ5i6usaOy",
+      id: "L6CG1_7Qxh",
       type: "paragraph",
       data: {
-        text: "1-Apoyar y colaborar con el desarrollo de los diferentes proyectos que se tienen con los clientes de la",
+        text: 'This certification is issued upon the request of <font style="color: rgb(0, 112, 255);">{{name}}</font> for whatever',
       },
       tunes: { anyTuneName: { alignment: "left" } },
     },
     {
-      id: "hBL1yS3-zX",
+      id: "lj9uURsb2s",
       type: "paragraph",
-      data: { text: "compañía." },
+      data: { text: "purpose it may serve him." },
       tunes: { anyTuneName: { alignment: "left" } },
     },
-    {
-      id: "nBtW1kAsrr",
-      type: "paragraph",
-      data: {
-        text: "2-Cooperar y participar en la creación y progreso de aplicaciones que se tienen con los clientes de la",
-      },
-      tunes: { anyTuneName: { alignment: "left" } },
-    },
-    {
-      id: "yYTn3ErFET",
-      type: "paragraph",
-      data: { text: "compañía." },
-      tunes: { anyTuneName: { alignment: "left" } },
-    },
-    {
-      id: "HOMezDRh0j",
-      type: "paragraph",
-      data: {
-        text: "3- Desarrollar el código fuente en el proyecto asignado.",
-      },
-      tunes: { anyTuneName: { alignment: "left" } },
-    },
-    {
-      id: "djGeVauWyL",
-      type: "paragraph",
-      data: {
-        text: "4- Prestar asistencia y soporte técnico de plataformas web y móvil.",
-      },
-      tunes: { anyTuneName: { alignment: "left" } },
-    },
-    {
-      id: "_OTmH4EL50",
-      type: "paragraph",
-      data: { text: "5- Realizar actividades de servicio al cliente." },
-      tunes: { anyTuneName: { alignment: "left" } },
-    },
-    {
-      id: "Fzm7MWSIpX",
-      type: "paragraph",
-      data: {
-        text: "6- Las demás actividades de desarrollo de tecnología requeridas, junto con aquellas actividades inherentesa la naturaleza del servicio prestado.",
-      },
-      tunes: { anyTuneName: { alignment: "left" } },
-    },
-    { id: "8gd6bFl5QE", type: "breakLine", data: { divider: false } },
+    { id: "JPg7k77mL1", type: "breakLine", data: { divider: false } },
     {
       id: "xqCyNLT8Wr",
       type: "image",
@@ -124,46 +52,20 @@ export const DEFAULT_EDITOR_DATA = {
       },
     },
     {
-      id: "mJ4yxaMuKo",
-      type: "paragraph",
-      data: { text: "Luz María Quintero L." },
-      tunes: { anyTuneName: { alignment: "left" } },
-    },
-    {
-      id: "CMNtyY7KIc",
-      type: "paragraph",
-      data: { text: "Directora Administrativa" },
-      tunes: { anyTuneName: { alignment: "left" } },
-    },
-    { id: "HikMQkVu1T", type: "breakLine", data: { divider: false } },
-    {
-      id: "2XIcqVISk7",
+      id: "Uke7DXCfds",
       type: "paragraph",
       data: {
-        text: '<font style="color: rgb(24, 10, 214);">info@kommit.co</font>',
+        text: '<font style="color: rgb(0, 112, 255);">{{signer_name}}</font>',
       },
       tunes: { anyTuneName: { alignment: "center" } },
     },
     {
-      id: "WDqs__pOee",
-      type: "paragraph",
-      data: { text: "Manizales, Colombia" },
-      tunes: { anyTuneName: { alignment: "center" } },
-    },
-    {
-      id: "ksyJ_Y4bGG",
-      type: "paragraph",
-      data: { text: " Cel +57 310 398 1206" },
-      tunes: { anyTuneName: { alignment: "center" } },
-    },
-    { id: "uCklT14jTA", type: "delimiter", data: {}},
-    {
-      id: "HkVDP2Qfbf",
+      id: "r1r-64w6_j",
       type: "paragraph",
       data: {
-        text: '<font style="color: rgb(24, 10, 214);">&nbsp;www.kommit.co – +1 (561) 903-6261 - Boynton Beach, Florida 33435</font>',
+        text: '<font style="color: rgb(0, 112, 255);">{{signer_position}}</font>',
       },
-      tunes: { anyTuneName: { alignment: "left" } },
+      tunes: { anyTuneName: { alignment: "center" } },
     },
   ],
   version: "2.27.2",
